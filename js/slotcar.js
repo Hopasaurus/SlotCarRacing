4a752d54d2f6164525163392af4b90ff6865a580
David Hoppe <hopasaurus@gmail.com>
2016-02-20 09:57:49 -0600

    Fiddling with babylon and learning about how to control the stuff in the scene and duplicate objects.

--- js/slotcar.js
+++ js/slotcar.js
@@ -3,13 +3,25 @@ if (BABYLON.Engine.isSupported()) {
     var engine = new BABYLON.Engine(canvas, true);
 
     BABYLON.SceneLoader.Load("", "BlenderFiles/SlotCar.babylon", engine, function (newScene) {
-        // Wait for textures and shaders to be ready
+	
+console.log(newScene);
+var car1 = _.find(newScene.meshes, function(mesh) { return mesh.name === "Car1"; });
+var newCar = car1.clone(car1.name);
+newCar.position.x += 2;
+newScene.meshes.push(newCar);
+
         newScene.executeWhenReady(function () {
-            // Attach camera to canvas inputs
+		
             newScene.activeCamera.attachControl(canvas);
 
-            // Once the scene is loaded, just register a render loop to render it
             engine.runRenderLoop(function() {
+		//fiddle with cars here...
+		//newScene.meshes[0].position.x += .001;
+		if(newScene.meshes[2].position.x < 1) {
+			newScene.meshes[2].position.x += .005;
+		}
+car1.rotation.y += .1;
+		newScene.meshes[2].rotation.y += .005;
                 newScene.render();
             });
         });
